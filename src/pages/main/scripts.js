f1f5422beffd2f20e97a03923da06196cc68897e
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2021-03-10 21:46:52 +0400

    10m21

---
+++ src/pages/main/scripts.js
@@ -0,0 +1,36 @@
+// burger menu
+
+let container = document.querySelector('.burger_js'),
+    stick = document.querySelector('.stick_js');
+
+let menuOpen = false;
+container.onclick = function () {
+    if (!menuOpen) {
+        stick.classList.add ('open');
+        menuOpen = true;
+    } else {
+        stick.classList.remove ('open');
+        menuOpen = false;
+    }
+}
+
+// open tabs
+
+// (function tabs () {
+//     let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+ 
+//      jsTriggers.forEach(function(trigger) {
+//          trigger.addEventListener('click', function () {
+//            let id = this.getAttribute('data-tab'),
+//                content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+//                activeTrigger = document.querySelector('.js-tab-trigger.active'),
+//                activeContent = document.querySelector('.js-tab-content.active');
+           
+//            activeTrigger.classList.remove('active');
+//            trigger.classList.add('active');
+           
+//            activeContent.classList.remove('active');
+//            content.classList.add('active');
+//          });
+//      });
+//    })();
