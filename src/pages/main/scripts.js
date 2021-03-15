b5376e442ffa00e2fa2f81ae0967149123d2a44d
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2021-03-15 20:03:38 +0400

    15m03

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,18 +1,23 @@
 // burger menu
 
-let container = document.querySelector('.burger_js'),
+(function burgerOpen () {
+
+    let container = document.querySelector('.burger_js'),
     stick = document.querySelector('.stick_js');
 
-let menuOpen = false;
-container.onclick = function () {
-    if (!menuOpen) {
-        stick.classList.add ('open');
-        menuOpen = true;
-    } else {
-        stick.classList.remove ('open');
-        menuOpen = false;
+    let menuOpen = false;
+    container.onclick = function () {
+        if (!menuOpen) {
+            stick.classList.add ('open');
+            menuOpen = true;
+        } else {
+            stick.classList.remove ('open');
+            menuOpen = false;
+        }
     }
-}
+})();
+
+
 
 // open tabs
 
@@ -34,3 +39,39 @@ container.onclick = function () {
 //          });
 //      });
 //    })();
+
+   //slider
+
+//    var elem = document.querySelector('.main-carousel');
+//    var flkty = new Flickity( elem, {
+//   // options
+//   cellAlign: 'left',
+//   contain: true
+// });
+
+(function slider () {
+    // let elem = document.querySelector('.main-carousel');
+    // var flktyA = new Flickity('.gallery-a');
+    // var flktyB = new Flickity( '.gallery-b', {
+    //     asNavFor: '.gallery-a'
+    // });
+    // let slideEl = new Flickity( elem, {
+    //   // options
+    //   imagesLoaded: true,
+    //   wrapAround: true,
+    //   autoPlay: 3000,
+    //   pauseAutoPlayOnHover: true,
+    //   pageDots: false
+    //   // fade: true
+    // });
+    var Flickity = require('flickity-as-nav-for');
+
+    var flktyA = new Flickity('.carousel-main');
+    var flktyB = new Flickity( '.carousel-nav', {
+      asNavFor: '.carousel-main'
+
+    });
+    
+  })();
+
+
