1744c611ead04ff111000e4583e950e369b2dba4
Vishnyakov Vitaliy <cherrytlt.j@gmail.com>
2021-03-16 16:49:56 +0400

    16m21

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -21,49 +21,29 @@
 
 // open tabs
 
-// (function tabs () {
-//     let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+(function tabs () {
+    let jsTriggers = document.querySelectorAll('.js-tab-trigger');
  
-//      jsTriggers.forEach(function(trigger) {
-//          trigger.addEventListener('click', function () {
-//            let id = this.getAttribute('data-tab'),
-//                content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
-//                activeTrigger = document.querySelector('.js-tab-trigger.active'),
-//                activeContent = document.querySelector('.js-tab-content.active');
+     jsTriggers.forEach(function(trigger) {
+         trigger.addEventListener('click', function () {
+           let id = this.getAttribute('data-tab'),
+               content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+               activeTrigger = document.querySelector('.js-tab-trigger.active'),
+               activeContent = document.querySelector('.js-tab-content.active');
            
-//            activeTrigger.classList.remove('active');
-//            trigger.classList.add('active');
+           activeTrigger.classList.remove('active');
+           trigger.classList.add('active');
            
-//            activeContent.classList.remove('active');
-//            content.classList.add('active');
-//          });
-//      });
-//    })();
+           activeContent.classList.remove('active');
+           content.classList.add('active');
+         });
+     });
+   })();
 
-   //slider
 
-//    var elem = document.querySelector('.main-carousel');
-//    var flkty = new Flickity( elem, {
-//   // options
-//   cellAlign: 'left',
-//   contain: true
-// });
 
 (function slider () {
-    // let elem = document.querySelector('.main-carousel');
-    // var flktyA = new Flickity('.gallery-a');
-    // var flktyB = new Flickity( '.gallery-b', {
-    //     asNavFor: '.gallery-a'
-    // });
-    // let slideEl = new Flickity( elem, {
-    //   // options
-    //   imagesLoaded: true,
-    //   wrapAround: true,
-    //   autoPlay: 3000,
-    //   pauseAutoPlayOnHover: true,
-    //   pageDots: false
-    //   // fade: true
-    // });
+
     var Flickity = require('flickity-as-nav-for');
 
     var flktyA = new Flickity('.carousel-main');
