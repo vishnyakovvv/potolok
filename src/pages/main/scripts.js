2f5c11dff37615eb8cf0ecaa30873f0d3eacc8c1
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2021-03-21 21:33:46 +0400

    21m21

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -55,3 +55,56 @@
   })();
 
 
+//   culc
+
+
+
+function calculate() {
+    let price = 0;
+    let room_length = document.calc.room_length.value;
+    let room_width = document.calc.room_width.value;
+    let tubes_quantity = document.calc.tubes_quantity.value;
+    let lightpoints_quantity = document.calc.lightpoints_quantity.value;
+    let svetilnik_quantity = document.calc.svetilnik_quantity.value;
+    let doroga = document.calc.doroga.value;
+    result_perimetr.innerHTML = room_width * 40;
+    result_tubes.innerHTML = tubes_quantity * 200;
+    result_lightpoints.innerHTML = lightpoints_quantity * 300;
+
+    if (doroga >= 0 && doroga <= 20) {
+        doroga_new = doroga * 0;
+    } else {
+        doroga_new = doroga * 12;
+    }
+    result_doroga.innerHTML = doroga_new;
+
+
+    if (svetilnik_quantity >= 0 && svetilnik_quantity <= 10) {
+        svetilnik_new = svetilnik_quantity * 290;
+    } else {
+        svetilnik_new = svetilnik_quantity * 270;
+    }
+    result_svetilnik.innerHTML = svetilnik_new;
+
+
+
+    if (room_length >= 0 && room_length <= 14) {
+        room_length_new = room_length * 290;
+    } else if (room_length >= 15 && room_length <= 20) {
+        room_length_new = room_length * 280;
+    } else if (room_length >= 21 && room_length <= 40) {
+        room_length_new = room_length * 260;
+    } else if (room_length >= 41) {
+        room_length_new = room_length * 240;
+    }
+    result_plochad.innerHTML = room_length_new;
+
+    price_new.innerHTML = room_length_new + svetilnik_new + doroga_new + document.calc.room_width.value * 40 + document.calc.tubes_quantity.value * 200 + document.calc.lightpoints_quantity.value * 300;
+}
+
+
+
+
+
+
+
