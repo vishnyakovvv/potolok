897429b993cbe0373bfa2c78c6245632a01cadfe
Vishnyakov Vitaliy <cherrytlt.j@gmail.com>
2021-03-22 16:52:43 +0400

    22m21

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,106 +1,154 @@
-// burger menu
+// // burger menu
 
-(function burgerOpen () {
+// (function burgerOpen () {
 
-    let container = document.querySelector('.burger_js'),
-    stick = document.querySelector('.stick_js');
+//     let container = document.querySelector('.burger_js'),
+//     stick = document.querySelector('.stick_js');
 
-    let menuOpen = false;
-    container.onclick = function () {
-        if (!menuOpen) {
-            stick.classList.add ('open');
-            menuOpen = true;
-        } else {
-            stick.classList.remove ('open');
-            menuOpen = false;
-        }
-    }
-})();
+//     let menuOpen = false;
+//     container.onclick = function () {
+//         if (!menuOpen) {
+//             stick.classList.add ('open');
+//             menuOpen = true;
+//         } else {
+//             stick.classList.remove ('open');
+//             menuOpen = false;
+//         }
+//     }
+// })();
 
 
 
-// open tabs
+// // open tabs
 
-(function tabs () {
-    let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+// (function tabs () {
+//     let jsTriggers = document.querySelectorAll('.js-tab-trigger');
  
-     jsTriggers.forEach(function(trigger) {
-         trigger.addEventListener('click', function () {
-           let id = this.getAttribute('data-tab'),
-               content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
-               activeTrigger = document.querySelector('.js-tab-trigger.active'),
-               activeContent = document.querySelector('.js-tab-content.active');
+//      jsTriggers.forEach(function(trigger) {
+//          trigger.addEventListener('click', function () {
+//            let id = this.getAttribute('data-tab'),
+//                content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+//                activeTrigger = document.querySelector('.js-tab-trigger.active'),
+//                activeContent = document.querySelector('.js-tab-content.active');
            
-           activeTrigger.classList.remove('active');
-           trigger.classList.add('active');
+//            activeTrigger.classList.remove('active');
+//            trigger.classList.add('active');
            
-           activeContent.classList.remove('active');
-           content.classList.add('active');
-         });
-     });
-   })();
+//            activeContent.classList.remove('active');
+//            content.classList.add('active');
+//          });
+//      });
+//    })();
 
 
 
-(function slider () {
+// (function slider () {
 
-    var Flickity = require('flickity-as-nav-for');
+//     var Flickity = require('flickity-as-nav-for');
 
-    var flktyA = new Flickity('.carousel-main');
-    var flktyB = new Flickity( '.carousel-nav', {
-      asNavFor: '.carousel-main'
+//     var flktyA = new Flickity('.carousel-main');
+//     var flktyB = new Flickity( '.carousel-nav', {
+//       asNavFor: '.carousel-main'
 
-    });
+//     });
     
-  })();
+// })();
 
 
-//   culc
+//   calc
 
-
-
-function calculate() {
-    let price = 0;
-    let room_length = document.calc.room_length.value;
-    let room_width = document.calc.room_width.value;
-    let tubes_quantity = document.calc.tubes_quantity.value;
-    let lightpoints_quantity = document.calc.lightpoints_quantity.value;
-    let svetilnik_quantity = document.calc.svetilnik_quantity.value;
-    let doroga = document.calc.doroga.value;
-    result_perimetr.innerHTML = room_width * 40;
-    result_tubes.innerHTML = tubes_quantity * 200;
-    result_lightpoints.innerHTML = lightpoints_quantity * 300;
-
-    if (doroga >= 0 && doroga <= 20) {
-        doroga_new = doroga * 0;
-    } else {
-        doroga_new = doroga * 12;
-    }
-    result_doroga.innerHTML = doroga_new;
-
-
-    if (svetilnik_quantity >= 0 && svetilnik_quantity <= 10) {
-        svetilnik_new = svetilnik_quantity * 290;
-    } else {
-        svetilnik_new = svetilnik_quantity * 270;
-    }
-    result_svetilnik.innerHTML = svetilnik_new;
-
-
-
-    if (room_length >= 0 && room_length <= 14) {
-        room_length_new = room_length * 290;
-    } else if (room_length >= 15 && room_length <= 20) {
-        room_length_new = room_length * 280;
-    } else if (room_length >= 21 && room_length <= 40) {
-        room_length_new = room_length * 260;
-    } else if (room_length >= 41) {
-        room_length_new = room_length * 240;
-    }
-    result_plochad.innerHTML = room_length_new;
-
-    price_new.innerHTML = room_length_new + svetilnik_new + doroga_new + document.calc.room_width.value * 40 + document.calc.tubes_quantity.value * 200 + document.calc.lightpoints_quantity.value * 300;
-}
+(function ($) {
+    $(function () {
+   
+          calculate();
+   
+          jQuery('#calculator input').keyup(function() {
+              this.value = this.value.replace(/[^0-9\.,]/g, '');
+              this.value = this.value.replace(/[,]/, '.');
+          });
+          jQuery('#calculator input, #calculator select').change(function() {
+              calculate();
+          });
+          jQuery('#calculator input').keyup(function() {
+              calculate();
+          });
+          function calculate() {
+        $('.calculator').each(function(key, val){
+          calcInputs = {};
+              $(this).find('input, select').each(function(key, val){
+                  name = $(this).attr('name');
+                  val = $(this).val();
+                  if (!$.isNumeric(val)) {
+                      switch (name) {
+                          case 'area':
+                              val = 1;
+                              break;
+                          case 'corners':
+                              val = 4;
+                              break;
+                          case 'lamp':
+                              val = '';
+                              break;
+                          case 'tube':
+                              val = '';
+                              break;
+                          case 'chandelier-hook':
+                              val = '';
+                              break;
+                          default:
+                              break;
+                      }
+                      $(this).val(val);
+                  }
+                  calcInputs[name] = val;
+              });
+   
+              total = 0;
+   
+              switch (calcInputs.texture) {
+                  case 'mat':
+              total += calcInputs.area * 113;
+              break;
+                  case 'glossy':
+              total += calcInputs.area * 180;
+              break;
+                  case 'satin':
+              total += calcInputs.area * 180;
+              break;
+                  case 'fabric':
+              total += calcInputs.area * 900;
+              break;
+              default:
+              break;
+              }
+   
+              // каждый угол
+                  total += calcInputs.corners * 40;
+   
+              // установка люстры
+              if (calcInputs['chandelier-hook'] >= 1) {
+                  total += calcInputs['chandelier-hook'] * 500;
+              }
+              // установка встраемого светильника
+              if (calcInputs.lamp >= 1) {
+                  total += calcInputs.lamp * 350;
+              }
+              // обход трубы
+              if (calcInputs.tube >= 1) {
+                  total += calcInputs.tube * 300;
+              }
+   
+          install = calcInputs.area * 170;
+          total += install;
+   
+              total += ' руб.';
+              jQuery(this).find('span.total').html(total);
+        });
+          }
+   
+      });
+  })(jQuery);
+  ;
 
 
 
