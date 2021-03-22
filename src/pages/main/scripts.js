6cc8bd89411dbd3631204d5ca262891460e969f1
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2021-03-22 21:55:12 +0400

    22m21

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,154 +1,82 @@
-// // burger menu
+//   calc
 
-// (function burgerOpen () {
+// (function calcCeil() {
+//     var colors = document.getElementById('#colors').value;
+//     var space = document.getElementById('#space').value;
+//     var angle = document.getElementById('#angle').value;
+//     var hole = document.getElementById('#hole').value;
+//     var lamp = document.getElementById('#lamp').value;
+//     var ceilost = colors * space + angle * 100 + hole * 100 + lamp * 500
+//     document.getElementById('#result') = ceilost
 
-//     let container = document.querySelector('.burger_js'),
-//     stick = document.querySelector('.stick_js');
 
-//     let menuOpen = false;
-//     container.onclick = function () {
-//         if (!menuOpen) {
-//             stick.classList.add ('open');
-//             menuOpen = true;
-//         } else {
-//             stick.classList.remove ('open');
-//             menuOpen = false;
-//         }
-//     }
-// })();
+//   })();
+
+//   console.log(colors);
+//   console.log(space);
+//   console.log(angle);
+//   console.log(hole);
+//   console.log(lamp);
+//   console.log(ceilost);
+
+// burger menu
+
+(function burgerOpen () {
+
+    let container = document.querySelector('.burger_js'),
+    stick = document.querySelector('.stick_js');
+
+    let menuOpen = false;
+    container.onclick = function () {
+        if (!menuOpen) {
+            stick.classList.add ('open');
+            menuOpen = true;
+        } else {
+            stick.classList.remove ('open');
+            menuOpen = false;
+        }
+    }
+})();
 
 
 
 // // open tabs
 
-// (function tabs () {
-//     let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+(function tabs () {
+    let jsTriggers = document.querySelectorAll('.js-tab-trigger');
  
-//      jsTriggers.forEach(function(trigger) {
-//          trigger.addEventListener('click', function () {
-//            let id = this.getAttribute('data-tab'),
-//                content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
-//                activeTrigger = document.querySelector('.js-tab-trigger.active'),
-//                activeContent = document.querySelector('.js-tab-content.active');
+     jsTriggers.forEach(function(trigger) {
+         trigger.addEventListener('click', function () {
+           let id = this.getAttribute('data-tab'),
+               content = document.querySelector('.js-tab-content[data-tab="'+id+'"]'),
+               activeTrigger = document.querySelector('.js-tab-trigger.active'),
+               activeContent = document.querySelector('.js-tab-content.active');
            
-//            activeTrigger.classList.remove('active');
-//            trigger.classList.add('active');
+           activeTrigger.classList.remove('active');
+           trigger.classList.add('active');
            
-//            activeContent.classList.remove('active');
-//            content.classList.add('active');
-//          });
-//      });
-//    })();
+           activeContent.classList.remove('active');
+           content.classList.add('active');
+         });
+     });
+   })();
 
 
 
-// (function slider () {
+(function slider () {
 
-//     var Flickity = require('flickity-as-nav-for');
+    var Flickity = require('flickity-as-nav-for');
 
-//     var flktyA = new Flickity('.carousel-main');
-//     var flktyB = new Flickity( '.carousel-nav', {
-//       asNavFor: '.carousel-main'
+    var flktyA = new Flickity('.carousel-main');
+    var flktyB = new Flickity( '.carousel-nav', {
+      asNavFor: '.carousel-main'
 
-//     });
+    });
     
-// })();
+})();
 
 
-//   calc
 
-(function ($) {
-    $(function () {
-   
-          calculate();
-   
-          jQuery('#calculator input').keyup(function() {
-              this.value = this.value.replace(/[^0-9\.,]/g, '');
-              this.value = this.value.replace(/[,]/, '.');
-          });
-          jQuery('#calculator input, #calculator select').change(function() {
-              calculate();
-          });
-          jQuery('#calculator input').keyup(function() {
-              calculate();
-          });
-          function calculate() {
-        $('.calculator').each(function(key, val){
-          calcInputs = {};
-              $(this).find('input, select').each(function(key, val){
-                  name = $(this).attr('name');
-                  val = $(this).val();
-                  if (!$.isNumeric(val)) {
-                      switch (name) {
-                          case 'area':
-                              val = 1;
-                              break;
-                          case 'corners':
-                              val = 4;
-                              break;
-                          case 'lamp':
-                              val = '';
-                              break;
-                          case 'tube':
-                              val = '';
-                              break;
-                          case 'chandelier-hook':
-                              val = '';
-                              break;
-                          default:
-                              break;
-                      }
-                      $(this).val(val);
-                  }
-                  calcInputs[name] = val;
-              });
-   
-              total = 0;
-   
-              switch (calcInputs.texture) {
-                  case 'mat':
-              total += calcInputs.area * 113;
-              break;
-                  case 'glossy':
-              total += calcInputs.area * 180;
-              break;
-                  case 'satin':
-              total += calcInputs.area * 180;
-              break;
-                  case 'fabric':
-              total += calcInputs.area * 900;
-              break;
-              default:
-              break;
-              }
-   
-              // каждый угол
-                  total += calcInputs.corners * 40;
-   
-              // установка люстры
-              if (calcInputs['chandelier-hook'] >= 1) {
-                  total += calcInputs['chandelier-hook'] * 500;
-              }
-              // установка встраемого светильника
-              if (calcInputs.lamp >= 1) {
-                  total += calcInputs.lamp * 350;
-              }
-              // обход трубы
-              if (calcInputs.tube >= 1) {
-                  total += calcInputs.tube * 300;
-              }
-   
-          install = calcInputs.area * 170;
-          total += install;
-   
-              total += ' руб.';
-              jQuery(this).find('span.total').html(total);
-        });
-          }
-   
-      });
-  })(jQuery);
-  ;
 
 
 
