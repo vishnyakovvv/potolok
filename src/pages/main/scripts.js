4d974c29bd5057503b30d04f112c6708326e2b3a
vishnyakovvv <45771541+vishnyakovvv@users.noreply.github.com>
2021-03-28 22:34:38 +0400

    28m21

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -40,6 +40,67 @@
      });
    })();
 
+
+  // window open
+
+(function windowOpen() {
+    let body = document.querySelector("body"),
+        callback = document.querySelector(".modal_js"),
+        buttonOpen = document.querySelectorAll(".js-open-modal"),
+        buttonClose = document.querySelector(".js-modal-close"),
+        inputText = document.querySelector(".input-text-callback_js");
+
+        console.log(callback);
+
+    buttonOpen.forEach(function(item) {
+      item.addEventListener("click", openCallbackPopup);
+
+      function openCallbackPopup() {
+  
+  
+          callback.classList.add("callback_open");
+          body.style.overflow = "hidden";
+          inputText.focus();
+  
+          buttonClose.addEventListener("click", closeCallbackPopup);
+  
+          window.addEventListener("keydown", function (event) {
+              if (event.code === "Escape") {
+                  closeCallbackPopup()
+              } 
+          });
+  
+          callback.addEventListener("click", function (event) {
+              if (event.target === callback) {
+                  closeCallbackPopup()
+              }
+          });
+      }
+    });
+
+    function closeCallbackPopup() {
+        callback.classList.remove("callback_open");
+        body.style.overflow = "unset";
+        buttonClose.removeEventListener("click", closeCallbackPopup);
+    }
+})();
+
+// menu
+
+var menuBtn = document.querySelector('.menu-btn');
+var nav = document.querySelector('.nav');
+var lineOne = document.querySelector('.nav .menu-btn .line--1');
+var lineTwo = document.querySelector('.nav .menu-btn .line--2');
+var lineThree = document.querySelector('.nav .menu-btn .line--3');
+var link = document.querySelector('.nav .nav-links');
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('.nav-open');
+    lineOne.classList.toggle('line-cross');
+    lineTwo.classList.toggle('line-fade-out');
+    lineThree.classList.toggle('line-cross');
+    link.classList.toggle('fade-in');
+})
+
 // slider
 
 (function slider () {
@@ -51,6 +112,8 @@
       asNavFor: '.carousel-main'
     });
 })();
+
+
 
 
 
